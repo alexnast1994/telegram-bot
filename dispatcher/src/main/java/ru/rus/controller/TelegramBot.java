3077df29af0429d9bc6b3f51d85ef7aec00c728b
alexnast1994 <105260572+alexnast1994@users.noreply.github.com>
2023-12-08 12:25:57 +0700

    Add files via upload

---
+++ dispatcher/src/main/java/ru/rus/controller/TelegramBot.java
@@ -0,0 +1,34 @@
+package ru.rus.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+    private static final Logger log = Logger.getLogger(TelegramBot.class);
+
+    @Value("${bot.name)")
+    private String botName;
+    @Value("${bot.token)")
+    private String botToken;
+
+    @Override
+    public String getBotToken() {
+        return botToken;
+    }
+
+    @Override
+    public String getBotUsername() {
+        return botName;
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        var originMessage = update.getMessage();
+        log.debug(originMessage.getText());
+    }
+}
